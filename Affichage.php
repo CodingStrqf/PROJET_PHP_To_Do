39a9033e2898376036996d7121b146845bbf219d
CodingStrqf <l.ruy42@orange.fr>
2021-11-13 11:21:07 +0100

    Affichage erreur

--- Affichage.php
+++ Affichage.php
@@ -1,2 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <h1>
+        To Do List
+    </h1>
+    <
+</head>
+<body>
+
+</body>
+
+</html>
 <?php
-    echo "test";
+
+    foreach ($TTache as $value ){
+        echo $value->__toString().'<br>';
+    }
