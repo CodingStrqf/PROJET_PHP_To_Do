e682c98519aca1401c3b01c866632149463e5dae
Adrien Denonfoux <adriendenonfoux3005didi@gmail.com>
2021-11-13 11:16:13 +0100

    Test des commit push et update avec phpStrom

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+require("Tache.php");
+
+$t1 = new Tache("Faire dodo","14/11/2021","rouge");
+$t2 = new Tache("Jouer à Valorant","14/11/2021","orange");
+$t3 = new Tache("dire bonjour à la mère à Léo","14/11/2021","vert");
+
+
+$TTache=[
+    "1"=>$t1,
+    "2"=>$t2,
+    "3"=>$t3
+];
+
+require("Affichage.php");
